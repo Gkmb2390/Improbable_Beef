4f916afaa4a6f4090f48a581371122942367cc13
Kevin Blum <gkmb23@Gmail.com>
2022-03-08 08:41:08 -0600

    12.3.1

--- 12-2-2/data-12-2-2-resources/plot.js
+++ 12-2-2/data-12-2-2-resources/plot.js
@@ -3,9 +3,23 @@ console.log(cityGrowths);
 var sortedCities = cityGrowths.sort((a,b) =>
 a.Increase_from_2016 - b.Increase_from_2016).reverse(); 
 
-var top5cities = sortedCities.slice(0,5)
+var top5cities = sortedCities.slice(0,7)
 
 var topFiveCityNames = top5cities.map(city=> city.City)
 
 var topFiveCityGrowths = top5cities.map(city => parseInt(city.Increase_from_2016));
 
+var trace = { 
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+};
+
+var data = [trace];
+var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: {title: "City"},
+    yaxis: {title: "Population Growth, 2016-2017"}
+};
+
+Plotly.newPlot("bar-plot", data, layout)
