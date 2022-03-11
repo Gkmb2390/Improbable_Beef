a6269b145c889cb2790a6ccf6d209c9c79ac8bfa
Kevin Blum <gkmb23@Gmail.com>
2022-03-10 18:42:23 -0600

    12.5.1

--- data-12-3-2-resources/plot.js
+++ data-12-3-2-resources/plot.js
@@ -23,3 +23,4 @@ d3.json("samples.json").then(function(data){
     firstPerson = data.metadata[0];
     Object.entries(firstPerson).forEach(([key, value])=> {console.log(key + ':' + value);});
 });
+
